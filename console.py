af3806b1eaeaf059ff846a5c632a64ec329f9671
victor kipchumba <victorkipchumba039@gmail.com>
2024-01-14 05:48:43 +0300

    Task 7: console 0.1

--- console.py
+++ console.py
@@ -3,6 +3,12 @@
     command interpreter for data creation and validation
 """
 import cmd
+import json
+from models import storage
+from models.base_model import BaseModel
+
+
+class_list = {'BaseModel': BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,12 +24,48 @@ class HBNBCommand(cmd.Cmd):
         """Overrides emptyline so as to do nothing"""
         pass
 
+    def do_create(self, arg):
+        """Crestes an instance of a class, saves and print id"""
+        try:
+            if not arg:
+                raise ValueError("** class name missing **")
+            if arg not in class_list:
+                raise TypeError("** class doesn't exist **")
+            my_object = eval(arg)()
+            my_object.save()
+            print(my_object.id)
+
+        except Exception as e:
+            print(e)
+
+    def do_show(self, arg):
+        """Prints a string representation of an nstance"""
+        try:
+            if not arg:
+                raise ValueError("** class name missing **")
+            else:
+                arguments = arg.split()
+                if len(arguments) != 2:
+                    raise ValueError("** instance id missing **")
+                elif arguments[0] not in class_list:
+                    raise ValueError("** class doesn't exist **")
+                else:
+                    for key, value in storage.all().items():
+                        if arguments[1] == value.id:
+                            print(value)
+                            return
+                    raise TypeError("** no instance found **")
+
+        except Exception as e:
+            print(e)
+
     def do_quit(self, arg):
         """Quit command exits the command interpreter"""
         return True
 
     def do_EOF(self, arg):
         """Handles End-Of-File"""
+        print()
         return True
 
 
