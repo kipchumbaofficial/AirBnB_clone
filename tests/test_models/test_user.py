245f736e6a34d2b121171dd162a8396ceca2136b
victor kipchumba <victorkipchumba039@gmail.com>
2024-01-17 03:55:55 +0300

    Task 8: More tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -13,3 +13,15 @@ class TestUser(unittest.TestCase):
     def test_email(self):
         """Tests email of user"""
         self.assertTrue(isinstance(User.email, str))
+
+    def test_password(self):
+        """Tests User Password"""
+        self.assertTrue(isinstance(User.password, str))
+
+    def test_first_name(self):
+        """Tests User First name"""
+        self.assertTrue(isinstance(User.first_name, str))
+
+    def test_last_name(self):
+        """Tests User Last Name"""
+        self.assertTrue(isinstance(User.last_name, str))
